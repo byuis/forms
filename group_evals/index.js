5fe670c51f7850d23e52be5cc4e0fb727b5192d6
Gove Allen <gove.allen@gmail.com>
2021-12-07 06:50:53 -0700

    Progress

--- group_evals/index.js
+++ group_evals/index.js
@@ -1,12 +1,15 @@
-evaluation=' <select name="a"><option value=1>1-Much less<br>than average</option><option value=2>2</option><option value=3>3-About average</option><option value=4>4</option><option value=5>5-Much more than average</option>'
+evaluation='<select name="a"><option value=1>1-Much less<br>than average</option><option value=2>2</option><option value=3>3-About average</option><option value=4>4</option><option value=5>5-Much more than average</option>'
 let team
-let evaluations
+const questionnaire = []
 
 function configure(){
     const params = atob(window.location.search.substr(1)).split("|")
+    document.getElementById("title").innerHTML=params[3]
+    document.getElementById("form_title").value=params[3]
     document.getElementById("netid").value=params[0]
     team=params[1].split(",")
     document.getElementById("student_name").innerHTML = team[0]
+    
 
     // remove null from the team.  Null is used because normal team size is 4, but when a team member drops the core, it is easier to substitute with null rather than reduce teh call to only 3 team members 
     for(let i=team.length-1;i>-1;i--){
@@ -36,6 +39,7 @@ function configure(){
     Promise.all(promises).then((question_sets) => {
         //now we have the question sets, get the labels
         for(const question_set of question_sets){
+            questionnaire.push(question_set)
             console.log(question_set.header)
             for(const question of question_set.questions){
                 console.log(question)   
@@ -59,54 +63,56 @@ function configure(){
             }
             // now we have the labels, ready to render            
 
+            //interate labels into questionnaire
+            for(const qs of questionnaire){
+                for(const q of qs.questions){
+                    q.labels = question_labels[q.labels]
+                }
+            }
+            console.log("===========================================")
+            console.log("Questionnaire", questionnaire)
+    
            
-
-           
-            for(const question_set of question_sets){
+            for(let q=0;q<questionnaire.length;q++){
+                const question_set = questionnaire[q]
                 const newdiv=document.createElement("div")
                 newdiv.className="header"
                 newdiv.innerHTML="<h2>" + question_set.header + "</h2>"
                 document.getElementById("evals").appendChild(newdiv);
 
                 for(let e=0; e < question_set.questions.length; e++){
-                    const one_eval = question_set.questions[e].text
+                    const question = question_set.questions[e]
                     const table=document.createElement('table')
                     const row=table.insertRow() 
                     const row_html=['<th class="first-col"></th>']
-                    console.log("question_labels",question_labels)
-                    console.log("question_set.questions[e]",question_set.questions[e])
-                    console.log("question_labels[question_set.questions[e].labels]",question_labels[question_set.questions[e].labels])
-                    for(const label of question_labels[question_set.questions[e].labels]){
-                        console.log("labelx", label)
+                    for(const label of question.labels){
                         if(label){
                             row_html.push("<th>" + label + "</th>")
                         }else{
                             row_html.push("<th></th>")
                         }
-                        
                     }
                     row.innerHTML=row_html.join("")
-            
                     for(let i=team.length-1;i>-1;i--){
                  
                         const row  = table.insertRow()
                         row.className="normal"
-                        row.id="row" + i + "q" + e
+                        row.id=`row${i}q${q}e${e}`
                         let suffix=""
                         if(i===0){suffix=" (you)"}
             
                         row.innerHTML=` <td class="first-col">${team[i]}${suffix}</td>
-                        <td><input type="radio" value="1" name="tm${i}q${e}" /></td>
-                        <td><input type="radio" value="2" name="tm${i}q${e}" /></td>
-                        <td><input type="radio" value="3" name="tm${i}q${e}" /></td>
-                        <td><input type="radio" value="4" name="tm${i}q${e}" /></td>
-                        <td><input type="radio" value="5" name="tm${i}q${e}" /></td>`
+                        <td><input type="radio" value="1" name="tm${i}q${q}e${e}" /></td>
+                        <td><input type="radio" value="2" name="tm${i}q${q}e${e}" /></td>
+                        <td><input type="radio" value="3" name="tm${i}q${q}e${e}" /></td>
+                        <td><input type="radio" value="4" name="tm${i}q${q}e${e}" /></td>
+                        <td><input type="radio" value="5" name="tm${i}q${q}e${e}" /></td>`
                     }
             
                     const evals=document.getElementById("evals")
                     const newdiv=document.createElement("div")
                     newdiv.innerHTML=`
-                        <h3><span class="emphasis">${one_eval}</span>.</h3>
+                        <h3><span class="emphasis">${question.text}</span>.</h3>
                     `
                     evals.appendChild(newdiv);
                     evals.appendChild(table);
@@ -127,31 +133,41 @@ function submit_form(){
     let message=null
     let data_comment
 
-    for(let e=0; e < evaluations.length; e++){
-        const ev = evaluations[e]
-        data[ev]={}
-        for(let i=team.length-1;i>-1;i--){
-            if (window.event.ctrlKey) {
-                //for debugging
-                data[ev][team[i]]=randBetween(1,5)
-            }else{
-                data[ev][team[i]]=null
-            }
-            for(const btn of document.getElementsByName(`tm${i}q${e}`)){
-                if(btn.checked){
-                    data[ev][team[i]]=parseInt(btn.value)
-                   //console.log("name of button:",btn.name, btn.value)
-                    document.getElementById("row" + i + "q" + e).className="normal"
+    for(let i=0;i<questionnaire.length;i++){
+        const qs = questionnaire[i]
+        data[qs.header] = {}
+        for(let j=0;j<qs.questions.length;j++){
+            const q = qs.questions[j]
+            data[qs.header][q.text]={}
+            for(let k=0;k<team.length;k++){
+                const tm = team[k]
+
+                if (window.event.ctrlKey) {
+                    //for debugging
+                    data[qs.header][q.text][tm]=randBetween(1,5)
+                }else{
+                    data[qs.header][q.text][tm]=null
                 }
-            }
-           //console.log("checking", e, i, data[ev][team[i]])
-            if(data[ev][team[i]]===null){
-               //console.log("null found at ", e, i)
-                message="Please complete all evaluations"
-                document.getElementById("row" + i + "q" + e).className="error"
+
+                // settign the value by scanning the radio buttons
+                for(const btn of document.getElementsByName(`tm${k}q${i}e${j}`)){
+                    if(btn.checked){
+                        data[qs.header][q.text][tm]=parseInt(btn.value)
+                        document.getElementById(`row${k}q${i}e${j}`).className="normal"
+                    }
+                }
+
+                if(data[qs.header][q.text][tm]===null){
+                    message="Please complete all evaluations"
+                    document.getElementById(`row${k}q${i}e${j}`).className="error"
+                }
             }
         }
     }
+
+     console.log("data", data)
+    // return false
+
 
     if(message){
         alert(message)
